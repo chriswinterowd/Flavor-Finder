5f90b5d0b40cbedfedf8a9ed1ddc4a85448fb816
unknown <chriswinterowd@gmail.com>
2025-01-22 03:02:33 -0800

    Update ApplicationDbContext: add Recipe entity with Fluent API configuration.

--- server/Data/ApplicationDbContext.cs
+++ server/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using FlavorFinder.Models.Entities;
 
 namespace FlavorFinder.Data
 {
@@ -9,6 +10,35 @@ namespace FlavorFinder.Data
             : base(options)
         {
 
+        }
+
+        public DbSet<RecipeEntity> Recipes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<RecipeEntity>(entity =>
+            {
+                entity.Property(r => r.Id)
+                    .IsRequired()
+                    .ValueGeneratedNever();
+
+                entity.Property(r => r.Title)
+                    .IsRequired();
+
+                entity.Property(r => r.Cuisines)
+                    .HasColumnType("jsonb");
+
+                entity.Property(r => r.DishTypes)
+                    .HasColumnType("jsonb");
+
+                entity.Property(r => r.ExtendedIngredients)
+                    .HasColumnType("jsonb");
+
+                entity.Property(r => r.AnalyzedInstructions)
+                    .HasColumnType("jsonb");
+            });
         }
     }
 }
